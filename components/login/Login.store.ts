81ce528d247fd93cbac7163600f28395ea23a271
superpiposo <nathanqueirozwork@gmail.com>
2024-11-09 21:28:49 -0300

    feat: sign stores

--- components/login/Login.store.ts
+++ components/login/Login.store.ts
@@ -1,14 +1,18 @@
 import { create } from "zustand";
 
 type state = {
-  email: string;
+  email?: string;
+  password?: string;
 };
 
 type action = {
   set_email: (email: string) => void;
+  set_password: (passoword: string) => void;
 };
 
 export const Login_Store = create<state & action>((set) => ({
-  email: "",
+  email: undefined,
   set_email: (email: string) => set({ email }),
+  password: undefined,
+  set_password: (password) => set({ password }),
 }));
--- components/signup/Signup.store.ts
+++ components/signup/Signup.store.ts
@@ -1,7 +1,15 @@
+import { CreateUser } from "@/services/database/user/User";
 import { create } from "zustand";
 
-type state = {};
+type state = {
+  create_user?: CreateUser;
+};
 
-type action = {};
+type action = {
+  set_create_user: (create_user: CreateUser | undefined) => void;
+};
 
-export const Signup_Store = create<state & action>((set) => ({}));
+export const Signup_Store = create<state & action>((set) => ({
+  create_user: undefined,
+  set_create_user: (create_user) => set({ create_user }),
+}));
