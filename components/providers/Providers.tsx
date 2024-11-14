6ee7587955dc7959ec12efa84291773513311d87
superpiposo <nathanqueirozwork@gmail.com>
2024-11-14 19:07:46 -0300

    Enhance: Providers

---
+++ components/providers/Providers.tsx
@@ -0,0 +1,20 @@
+"use client";
+import React, { ReactNode } from "react";
+import Wallet_Provider from "./wallet_provider/Wallet_Provider";
+import Transaction_Provider from "./transaction_provider/Transaction_Provider";
+import Insights_Provider from "./insights_provider/Insights_Provider";
+import Frases_Provider from "./frases_provider/Frases_Provider";
+
+export default function Providers({ children }: { children: ReactNode }) {
+  return (
+    <>
+      <Wallet_Provider>
+        <Transaction_Provider>
+          <Insights_Provider>
+            <Frases_Provider>{children}</Frases_Provider>
+          </Insights_Provider>
+        </Transaction_Provider>
+      </Wallet_Provider>
+    </>
+  );
+}
--- components/wallet/Wallet.tsx
+++ components/wallet/Wallet.tsx
@@ -7,66 +7,57 @@ import Item from "./parts/item/Item";
 import Controles from "./parts/controles/Controles";
 import Tool_Panel from "./parts/tool_panel/Tool_Panel";
 import Toggle_Theme from "../shared/Toggle_Theme";
-import Wallet_Provider from "../providers/wallet_provider/Wallet_Provider";
-import Transaction_Provider from "../providers/transaction_provider/Transaction_Provider";
 import { Transaction_Store } from "@/global_stores/Transaction.store";
 import { Wallet_Service } from "./Wallet.service";
-import Insights_Provider from "../providers/insights_provider/Insights_Provider";
-import Frases_Provider from "../providers/frases_provider/Frases_Provider";
+import Providers from "../providers/Providers";
 
 const wallet_service = new Wallet_Service();
 
 export default function Wallet() {
   const { transactions } = Transaction_Store();
   return (
-    <Wallet_Provider>
-      <Transaction_Provider>
-        <Insights_Provider>
-          <Frases_Provider>
-            <Container>
-              <Toggle_Theme />
-              <Form_Container>
-                <div className="flex-grow w-5/6 pt-10">
-                  <div className="w-full h-full flex flex-col rounded-md border-stone-300 dark:border-stone-700 overflow-hidden relative">
-                    <div
-                      className="w-full flex-grow bg-white dark:bg-stone-700 rounded-md border border-stone-300 
+    <Providers>
+      <Container>
+        <Toggle_Theme />
+        <Form_Container>
+          <div className="flex-grow w-5/6 pt-10">
+            <div className="w-full h-full flex flex-col rounded-md border-stone-300 dark:border-stone-700 overflow-hidden relative">
+              <div
+                className="w-full flex-grow bg-white dark:bg-stone-700 rounded-md border border-stone-300 
           flex flex-col gap-2 relative overflow-hidden"
-                    >
-                      <div className="flex flex-col gap-2 overflow-y-scroll basis-5/6 py-3">
-                        {transactions && transactions.length > 0 ? (
-                          transactions.map((transacao, index) => (
-                            <Item
-                              key={index}
-                              onClick={() => {
-                                wallet_service.delete_transaction(transacao.id);
-                              }}
-                              data={transacao.date.toString()}
-                              tipo_transacao={transacao.typeId}
-                              motivo={transacao.description}
-                              valor={Number(transacao.value)}
-                            />
-                          ))
-                        ) : (
-                          <div className="w-full h-30 px-3 ">
-                            <div
-                              className="border border-green bg-green w-full h-full rounded-md p-3 text-white
+              >
+                <div className="flex flex-col gap-2 overflow-y-scroll basis-5/6 py-3">
+                  {transactions && transactions.length > 0 ? (
+                    transactions.map((transacao, index) => (
+                      <Item
+                        key={index}
+                        onClick={() => {
+                          wallet_service.delete_transaction(transacao.id);
+                        }}
+                        data={transacao.date.toString()}
+                        tipo_transacao={transacao.typeId}
+                        motivo={transacao.description}
+                        valor={Number(transacao.value)}
+                      />
+                    ))
+                  ) : (
+                    <div className="w-full h-30 px-3 ">
+                      <div
+                        className="border border-green bg-green w-full h-full rounded-md p-3 text-white
                            justify-center items-center flex"
-                            >
-                              Não existem transações!
-                            </div>
-                          </div>
-                        )}
+                      >
+                        Não existem transações!
                       </div>
-                      <Tool_Panel />
                     </div>
-                  </div>
+                  )}
                 </div>
-                <Controles />
-              </Form_Container>
-            </Container>
-          </Frases_Provider>
-        </Insights_Provider>
-      </Transaction_Provider>
-    </Wallet_Provider>
+                <Tool_Panel />
+              </div>
+            </div>
+          </div>
+          <Controles />
+        </Form_Container>
+      </Container>
+    </Providers>
   );
 }
