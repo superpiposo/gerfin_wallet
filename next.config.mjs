3ffa3d0ecb8b4a8826d1f4d8047e391640018b2d
superpiposo <nathanqueirozwork@gmail.com>
2024-11-26 15:41:54 -0300

    cors

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,26 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    async headers() {
+        return [
+            {
+                source: "/api/:path*",
+                headers: [
+                    {
+                        key: "Access-Control-Allow-Origin",
+                        value: "https://gerfin-wallet-e41q54u6y-nathans-projects-869367ce.vercel.app/",
+                    },
+                    {
+                        key: "Access-Control-Allow-Methods",
+                        value: "GET, POST, PUT, DELETE, OPTIONS",
+                    },
+                    {
+                        key: "Access-Control-Allow-Headers",
+                        value: "Content-Type, Authorization",
+                    },
+                ],
+            },
+        ];
+    },
+};
 
 export default nextConfig;
